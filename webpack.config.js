f2030804d63bc8a1eab9f861277c7f925cb67de4
kristinamurgovska <kmurgovska@theearplatform.com>
2017-12-21 11:13:31 +0100

    Created webpack,package.json and gitignore files

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.jsx',
+    output: {
+        path: path.resolve('dist'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react', 'es2015', 'stage-3']
+                }
+            },
+            {
+                test: /\.(scss|css)$/,
+                loader: ExtractTextPlugin.extract('css-loader!sass-loader')
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif|svg|woff|woff2)$/,
+                loader: 'url-loader?limit=10000',
+            }, {
+                test: /\.(eot|ttf|wav|mp3)$/,
+                loader: 'file-loader',
+            }]
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: './src/index.html',
+        filename: 'index.html',
+        inject: 'body'
+    }),
+    new ExtractTextPlugin('dist/styles/main.css', {
+        allChunks: true
+    })],
+    devServer: {
+        historyApiFallback: true
+    }
+}
